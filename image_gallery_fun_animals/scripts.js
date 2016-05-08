f60dafb0e506f89bd2e11d7c0ae52b42c6cbb840
Lido Fernandez <lidofernandezburgos@gmail.com>
2016-05-09 01:07:39 +0200

    WIP

--- image_gallery_fun_animals/scripts.js
+++ image_gallery_fun_animals/scripts.js
@@ -1,9 +1,11 @@
 var animalAlbums,
-    animalList,
-    animalDetails;
+animalList,
+animalDetails;
 
-var currentAlbum = animalsData.category;
-var currentList = currentAlbum[0];
+var currentAlbum = animalsData.category[0];
+var currentList = currentAlbum.animals[0];
+var currentPhoto = currentList.image1;
+console.log(currentPhoto);
 
 function showTemplate(template, data){
   var html    = template(data);
@@ -12,17 +14,33 @@ function showTemplate(template, data){
 
 
 $(document).ready(function(){
+ // Compilers
 
   var source   = $('#albums-template').html();
   animalAlbums = Handlebars.compile(source);
+  source   = $('#animal-list-template').html();
+  animalList = Handlebars.compile(source);
+  source   = $('#animal-details-template').html();
+  animalDetails = Handlebars.compile(source);
 
+  // Templates
   $('#albums-tab').click(function () {
-
     showTemplate(animalAlbums, animalsData);
+    // $('.navbar-nav .active').removeClass('active');
+    // $('#albums-tab').addClass('active');
+    $(".album-thumbnail").click(function (){
+      var index = $(this).data("id");
+      currentAlbum = animalsData.category[index];
+      showTemplate(animalList, currentAlbum);
 
-    $('.navbar-nav .active').removeClass('active');
-    $('#albums-tab').addClass('active');
+      $(".album-single-photo").click(function (){
+        var index = $(this).data("id");
+        currentPhoto = currentList.image1;
+        console.log(currentList);
+        showTemplate(animalDetails, currentList);
+      });
 
+    });
   });
 
   $('#albums-tab').click();
